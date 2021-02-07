91a0f9d32f8b27bde46a9fafa94701fdd2458154
Jose Miguel C Evangelista <jomigevangelista117@gmail.com>
2021-02-07 17:02:47 +0800

    Push to github -initial commit

---
+++ variable.js
@@ -0,0 +1,39 @@
+// var, let, const
+
+var name = 'JUST JOMIG';
+console.log(name);
+name = 'Just Jomig';
+console.log(name);
+
+
+// Init var
+var greeting;
+console.log(greeting)
+greeting = 'Hi';
+console.log(greeting)
+
+// letters, number, _, $
+// Can not start with number
+
+// Multi word vars
+var firstName = 'Im'; // 1st
+var first_name = 'Just'; // 2nd
+var FirstName = 'Jomig'; // 3rd
+
+// Let
+// let name;
+//name = 'JUST JOMIG';
+//console.log(name);
+
+const person = {
+    name: 'Jomig',
+    age: 28
+};
+
+console.log(person);
+
+const numbers = ['x','y','z','a','b'];
+
+numbers.push('variables');
+
+console.log(numbers);
